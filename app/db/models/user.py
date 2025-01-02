42d18c46f39b1bdb7687dc3f48c508982ac21dc4
Daniel Santos <drsantos20@gmail.com>
2025-01-02 16:32:25 -0300

    chore: update user db model

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,13 +1,14 @@
 
 
-from sqlmodel import Column, SQLModel, Integer, String
+from typing import Optional
+from sqlmodel import Field, SQLModel
 
 
 class User(SQLModel, table=True):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True)
-    hashed_password = Column(String)
-
+    id: Optional[int] = Field(default=None, primary_key=True, index=True)
+    username: str = Field(unique=True, index=True)
+    email: str = Field(unique=True)
+    hashed_password: str
+    is_active: bool = Field(default=True)
